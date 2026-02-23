1982dfcd5537b7d2c1634ad7b715ac63072bbc56
Nikolay Efremov <efrem_nn@mail.ru>
2026-02-23 21:19:00 +0700

    Github Workflows fix url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ra-calendar/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ra-store-func/' : '/',
   plugins: [react()],
 })
